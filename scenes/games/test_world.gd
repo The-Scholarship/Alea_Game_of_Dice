4b9bb48ec2b9e9c34bedae6bf0ab1a6ab4d9e888
SpykieLucilius <spykie.lucilius@pm.me>
2026-04-18 16:45:07 +0300

    trying to fix the clipping issues with the wall

--- scenes/games/test_world.gd
+++ scenes/games/test_world.gd
@@ -57,6 +57,8 @@ func _mouse_to_world(screen_pos: Vector2) -> Vector3:
 func _process(delta):
 	if _grabbed:
 		var world_pos = _mouse_to_world(get_viewport().get_mouse_position())
+		world_pos.x = clamp(world_pos.x, -25.0, 25.0)
+		world_pos.z = clamp(world_pos.z, -25.0, 25.0)
 		_throw_vel = (world_pos - _last_world_pos) / delta
 		_last_world_pos = world_pos
 		die.global_position = world_pos
